42057d80999133a9c75445054dc691a17642cb65
jasonwillhudson <jasonwillhudson@gmail.com>
2022-09-12 01:10:10 -0700

    home page done

---
+++ src/team/HomePage.js
@@ -0,0 +1,14 @@
+const Home = () =>{
+
+    return <div className="home">
+        <h4>&copy;IAT 410</h4>
+        <h1>GAME NAME</h1>
+        <h2>Blog</h2>
+        <a>Group's</a>
+        <a>Daniel's</a>
+        <a>William's</a>
+        <a>Jason's</a>
+    </div>
+}
+
+export default Home;
