76fa7a6e6bebbf3820b4353784470a3fe434de44
jasonwillhudson <jasonwillhudson@gmail.com>
2022-09-22 23:48:08 -0700

    wasee week two added

--- src/wasee/WeekOne.js
+++ src/wasee/WeekOne.js
@@ -1,9 +1,13 @@
 const WeekOne = () => {
 
     return <div className="blog-content">
-        <h2>Week One</h2>
-        <h3>2022, Sep 11th</h3>
-        <p>Discuss about the game proposal and brainstorming the ideas.</p>
+        <h2>Week Two</h2>
+        <h3>2022, Sep 22nd</h3>
+        <p>After ideation and planning on week 1, I have taken the role of programming and artist because of my previous
+            experience in developing games. Week 2 went by without a lot of contribution from me. I am slowly starting
+            my way around Unity and Japanese mythology. I'm not well taught on mythology but I find it really
+            interesting. Personally researching on character and appearance to suite the characteristics of the Yokai, a
+            class of supernatural entity in Japanese folklore.</p>
 
     </div>
 }
